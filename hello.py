809fce5a49ff45c8901c91905d8bd25cf500d7cc
MichaelAbe <michael.k.abe@gmail.com>
2021-08-12 13:19:34 -0400

    simple area equation

--- hello.py
+++ hello.py
@@ -4,8 +4,14 @@
 
 #comments are just '#' hashed out 
 
-greeting = "Welcome, "
-name = input('Tell me your name: ')
-age = input('How old are you?')
-goals = input('What do you want to get out of learning Python?')
-print(greeting + name, 'you are', age, '.')
+# greeting = "Welcome, "
+# name = input('Tell me your name: ')
+# age = input('How old are you?')
+# goals = input('What do you want to get out of learning Python?')
+# print(greeting + name, 'you are', age, '.')
+
+# We will calculate the area of a circle
+
+radius = input('What is the radius of your circle?  ')
+answer = 3.14 * int(radius)**2
+print('The area of your circle is', answer)
