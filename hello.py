3e316a5cb3cf8afe4c4f5c13eb720ebfbfb915aa
MichaelAbe <michael.k.abe@gmail.com>
2021-08-19 14:27:06 -0400

    basic if statement

--- hello.py
+++ hello.py
@@ -16,8 +16,8 @@
 # answer = 3.14 * int(radius)**2
 # print('The area of your circle is', answer)
 
-num1 = 3.14
-num2 = 202.902
+# num1 = 3.14
+# num2 = 202.902
 
 # PREVIOUS
 # print('num1 is', num1, 'and num2 is', num2)
@@ -26,4 +26,14 @@ num2 = 202.902
 # print('num 1 is {0} and num 2 is {1}'.format(num1, num2))
 
 # USING F-STRINGS
-print(f'num 2 is {num1} and num 2 is {num2}')
+# print(f'num 2 is {num1} and num 2 is {num2}')
+
+# IF STATEMENTS
+age = int(input('Enter your age:'))
+if age < 10:
+    print('You are under 10 years old')
+elif age < 20:
+    print('You are older than ten but less than twenty years old')
+else: 
+    print('You are an old person')
+
