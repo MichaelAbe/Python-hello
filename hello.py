c77f476af90685a5c5125e1b28555d026a2222dc
MichaelAbe <michael.k.abe@gmail.com>
2021-08-26 11:58:25 -0400

    using continue in loop

--- hello.py
+++ hello.py
@@ -70,6 +70,10 @@ age = 25
 num = 0
 
 while num < age:
+    if num == 0:
+        num += 1
+        continue 
+        # continue basically skips this and continues to run the loop
     if num % 2 ==0:
         print(num)
     if num > 10:
